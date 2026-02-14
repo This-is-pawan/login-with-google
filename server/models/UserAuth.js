dd65f72f1c55d31f9c603f3d87407d8fd46d6f49
Pawan <154042595+This-is-pawan@users.noreply.github.com>
2026-02-14 13:39:47 +0530

    Update UserAuth.js

--- server/models/UserAuth.js
+++ server/models/UserAuth.js
@@ -1,43 +1,43 @@
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const GoogleUserSchema = new mongoose.Schema(
-//   {
-//     googleId: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       index: true,
-//     },
-//     name: {
-//       type: String,
-//       required: true,
-//       trim: true,
-//     },
-//     email: {
-//       type: String,
-//       required: true,
-//       unique: true,
-//       lowercase: true,
-//     },
-//     photo: {
-//       type: String,
-//       default: "",
-//     },
-//     refreshToken: {
-//       type: String,
-//       default: "",
-//     },
-//     role: {
-//       type: String,
-//       enum: ["user", "admin"],
-//       default: "user",
-//     },
-//     lastLogin: {
-//       type: Date,
-//       default: Date.now,
-//     },
-//   },
-//   { timestamps: true }
-// );
-// const GoogleUser=mongoose.model("GoogleUser", GoogleUserSchema);
-// module.exports = GoogleUser
+const GoogleUserSchema = new mongoose.Schema(
+  {
+    googleId: {
+      type: String,
+      required: true,
+      unique: true,
+      index: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    photo: {
+      type: String,
+      default: "",
+    },
+    refreshToken: {
+      type: String,
+      default: "",
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    lastLogin: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+const GoogleUser=mongoose.model("GoogleUser", GoogleUserSchema);
+module.exports = GoogleUser
